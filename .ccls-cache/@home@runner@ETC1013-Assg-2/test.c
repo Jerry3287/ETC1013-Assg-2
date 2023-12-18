ae3064b7d3c994878e95a0ff4a9de667811f1ebc
Jerry3827 <145307732+Jerry3827@users.noreply.github.com>
2023-12-18 06:20:32 +0000

    Matrix print function done

--- .ccls-cache/@home@runner@ETC1013-Assg-2/test.c
+++ .ccls-cache/@home@runner@ETC1013-Assg-2/test.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int **matCreate(int *matRow, int *matCol);
-void matPrint(int **matrix, int matDim[2]);
+int **matCreate(int *matRow, int *matCol, int *address); // matCreate prototype
+void matPrint(int **matrix, int matDim[2]); // matPrint prototype
+
 
 int main()
 {
     int mat1Size[2], mat2Size[2];
+    int address;
 
     printf("Please enter the two dimensions for 1st matrix\n"); //1st matrix
-    int **matrix1 = matCreate(&mat1Size[0], &mat1Size[1]);
+    int **matrix1 = matCreate(&mat1Size[0], &mat1Size[1], &address);
     printf("Please enter the two dimensions for 2nd matrix\n"); //2nd matrix
     int **matrix2 = matCreate(&mat2Size[0], &mat2Size[1]);
 
+  
+  
     return 1;
 }
 
@@ -20,10 +24,10 @@ int main()
   *Function: matCreate
   *Purpose: allocates memory and creates matrix
   *Param: matRow - size of the row
-  *      matCol - size of the column
+  *       matCol - size of the column
   *Return: matrix
 */
-int **matCreate(int *matRow, int *matCol)
+int **matCreate(int *matRow, int *matCol, int *address)
 {
     int row, column;
   
@@ -54,29 +58,31 @@ int **matCreate(int *matRow, int *matCol)
     }
   
     printf("the matrix that you have entered is:\n");
-    for (int i = 0; i < size[0]; i++)
-    {
-        for (int j = 0; j < size[1]; j++)
-        {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
+    matPrint(matrix, size);
 
     return matrix;
 }
 
+/*
+  *Function: matPrint
+  *Purpose: prints matrix
+  *Param: matrix - matrix to print
+  *       matDim - dimensions of the matrix
+  *Return: none
+*/
 void matPrint(int **matrix, int matDim[2])
 {
     int row = matDim[0];
     int column = matDim[1];
-  
+
     for (int i = 0; i < row; i++)
     {
+        printf("[");
         for (int j = 0; j < column; j++)
         {
-            printf("%d ", matrix[i][j]);
+            if (j != 0) printf(", ");
+            printf("%3d", matrix[i][j]);
         }
-        printf("\n");
+        printf("]\n");
     }
 }
